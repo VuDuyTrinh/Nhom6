dcca4adb959029b87f869424bedf20a64d575fb4
VuDuyTrinh <vuduytrinh.hutech@gmail.com>
2017-01-08 00:37:03 +0700

    khởi tạo
    vũ duy trình

---
+++ Nhom6/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Nhom6.Startup))]
+namespace Nhom6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
