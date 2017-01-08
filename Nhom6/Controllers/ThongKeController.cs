68c0eeef28f42b01b57ef2e263376c8daf2f48f5
Lê Chính <lxchinh89@gmail.com>
2017-01-08 13:50:38 +0700

    Update Controllers Thống kê
    Thống kê đơn đặt hàng

--- Nhom6/Controllers/ThongKeController.cs
+++ Nhom6/Controllers/ThongKeController.cs
@@ -23,5 +23,18 @@ namespace Nhom6.Controllers
             ViewBag.SL = i;
             return View(i);
         }
+        public ActionResult ddh()
+        {
+            var a = from i in data.DonDatHangs where (i.DaThanhToan == true) select i;
+            var b = from i in data.DonDatHangs where (i.TinhTrangGiaoHang == true) select i;
+            var c = from i in data.DonDatHangs where (i.DaThanhToan == true && i.TinhTrangGiaoHang == true) select i;
+            var d = from i in data.DonDatHangs where (i.DaThanhToan == false && i.TinhTrangGiaoHang == false) select i;
+            ViewBag.DDT = a.Count();
+            ViewBag.DGH = b.Count();
+            ViewBag.DTTGH = c.Count();
+            ViewBag.KTTGH = d.Count();
+            return View();
+
+        }
 	}
 }
