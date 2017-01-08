2b3fc3b35e2a848689b0096f3254f29446ce6097
vitcon272393 <badaopro.2011.nvt@gmail.com>
2017-01-08 14:35:44 +0700

    Chitiet DDH

--- Nhom6/Controllers/DDHController.cs
+++ Nhom6/Controllers/DDHController.cs
@@ -71,5 +71,33 @@ namespace Nhom6.Controllers
             var ds = from a in data.DonDatHangs where (a.TinhTrangGiaoHang == false || a.DaThanhToan == false) select a;
             return View(ds);
         }
+        public ActionResult ChiTietDDH(int id)
+        {       //kiem tra xem admin nếu có thực hiện kiểm tra quyền ở phần phân quyền
+            if (Session["Admin"] == null)
+            {
+                return RedirectToAction("Login", "Admin");
+            }
+            int co = 0;
+            String tencn = "";
+            //lấy các chức năng của tài khoản admin đăng nhập
+            List<CnAd> cn = Session["CN"] as List<CnAd>;
+            foreach (CnAd i in cn)
+            {
+                //kiểm tra xem tai khoản có quyền quản lý đơn đặt hàng k?
+                if (i.iAction == "DDH" && i.iController == "DDH")
+                {
+                    tencn = i.iTenCN;
+                    //nếu có gán biên bằng =1
+                    co = 1;
+                }
+            }
+            //kiểm tra biên cờ
+            if (co == 0)
+            {
+                return RedirectToAction("Loi", "Admin");
+            }
+            ddh dondathang = new ddh(id);
+            return View(dondathang);
+        }
     }
 }
