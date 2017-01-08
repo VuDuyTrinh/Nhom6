c22895b96f51d1da1ca4712b2b2e44f8bec5bc7d
vitcon272393 <badaopro.2011.nvt@gmail.com>
2017-01-08 13:55:45 +0700

    cnad.cs DDH.cs

---
+++ Nhom6/Controllers/DDHController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nhom6.Controllers
+{
+    public class DDHController : Controller
+    {
+        // GET: DDH
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
