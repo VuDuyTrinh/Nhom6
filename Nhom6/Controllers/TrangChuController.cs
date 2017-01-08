3cbbd95062a76fd301613bc6384aad7cb1478090
nguyen146 <shjratori@gmail.com>
2017-01-08 11:38:59 +0700

    Create Home page

--- Nhom6/Controllers/TrangChuController.cs
+++ Nhom6/Controllers/TrangChuController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList; 
+using PagedList.Mvc;
+using Nhom6.Models;
 
 namespace Nhom6.Controllers
 {
     public class TrangChuController : Controller
     {
-        // GET: TrangChu
-        public ActionResult Index()
+        QLNHDataContext data = new QLNHDataContext(); // Biến data quản lý CSDL
+        private List<SanPham> LaySP()
         {
-            return View();
+            return data.SanPhams.ToList(); // hàm trả về list table SanPham
         }
+
+        public ActionResult Index(int? page) // Khi nhấn vào trang chủ hàm này sẽ kích hoạt
+        {                                      //tham số page để chỉ ra số trang trong list sản phẩm ở trang chủ
+            int SoSP = 9; // SoSP chỉ ra số sản phẩm sẽ hiển thị trên web, nếu quá hơn số sp sẽ tự phân thành trang kế tiếp             
+            int SoTrang = (page ?? 1); //Chỉ ra số trang 
+
+            var nh = LaySP(); //Lấy list sản phẩm có trong CSDL
+            return View(nh.ToPagedList(SoTrang, SoSP)); //Trả về Model chứa table SanPham, chứa hàm phân trang có trong PagedList
+        }
+
     }
 }
